13ecad4a267b2887b647c116e26cbe33fbf815fc
Abhimanyu Rana <abhimanyu.rana@walmart.com>
2019-03-08 13:24:37 -0600

    Adding twoNumbers with all the edge cases

--- leetCode/src/LinkedList.java
+++ leetCode/src/LinkedList.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class LinkedList {
 
   private static Node head;
@@ -6,10 +9,24 @@ public class LinkedList {
   ////// Add at the end///////
 
   private void addAtTheEnd(int data) {
+
     if (head == null) {
       head = new Node(data);
     } else {
       Node p = head;
+      while (p.next != null) {
+        p = p.next;
+      }
+      p.next = new Node(data);
+    }
+  }
+
+  //// Creatign a new linkedList and adding elements/////
+  private void addAtTheEnd1(int data) {
+    if (head1 == null) {
+      head1 = new Node(data);
+    } else {
+      Node p = head1;
       while (p.next != null) {
         p = p.next;
       }
@@ -67,16 +84,50 @@ public class LinkedList {
     }
   }
 
-  /////
+  ///// Adding Two Numbers (Easy no edge case single element)/////
 
   private void addTwoNumbersEasy() {
 
     Node p = head;
     Node k = head1;
     int total = 0;
-
     while (p.next != null) {
       total += p.data + k.data;
+    }
+  }
+
+  private void addTwoNumbers() {
+    Node p = head;
+    Node k = head1;
+    int carry = 0;
+    int sum = 0;
+    ArrayList<Integer> out = new ArrayList<Integer>();
+
+    while (p != null) {
+      if (k != null) {
+        sum += carry + p.data + k.data;
+      } else {
+        sum += carry + p.data;
+      }
+      if (sum > 9) {
+        out.add(sum % 10);
+        carry = 1;
+        sum = 0;
+      } else {
+        out.add(sum);
+        carry = 0;
+        sum = 0;
+      }
+      p = p.next;
+    }
+    if (carry == 1) {
+      out.add(1);
+    }
+
+      Collections.reverse(out);
+
+    for (int i = 0; i < out.size(); i++) {
+      System.out.print(out.get(i));
     }
   }
 
@@ -93,6 +144,10 @@ public class LinkedList {
   public static void main(String[] args) {
 
     LinkedList ls = new LinkedList();
+    /*
+    ls.addAtTheEnd1(13);
+    ls.addAtTheEnd1(12);
+    ls.addAtTheEnd1(11);
     ls.addAtTheEnd(1);
     ls.addAtTheEnd(2);
     ls.addAtTheEnd(3);
@@ -100,6 +155,21 @@ public class LinkedList {
     ls.viewList(head);
     ls.removeStart();
     ls.viewList(head);
+    ls.viewList(head1);
+    */
+
+    //////// Add Two Numbers /////////
+
+    ls.addAtTheEnd(9);
+    ls.addAtTheEnd(9);
+    ls.addAtTheEnd(9);
+
+    ls.addAtTheEnd1(9);
+    ls.addAtTheEnd1(9);
+    ls.addAtTheEnd1(9);
+
+    ls.addTwoNumbers();
+
     System.out.println("Program Ended");
   }
 }
